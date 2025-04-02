5dc1fe81b637a1e443774236ecab9c6b94f8f5c9
aqib javed <aqibbutt3078@gmail.com>
2025-04-02 03:54:37 +0100

    EC2 User data script

--- ec2/userData/ec2-user-data.sh
+++ ec2/userData/ec2-user-data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 yum update -y
 yum install -y httpd
-
 systemctl start httpd
 systemctl enable httpd
 
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+echo "<h1>Eid Mubarak from $(hostname -f) </h1>" > /var/www/html/index.html
